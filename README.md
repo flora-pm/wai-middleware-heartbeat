4ac3d142be448da22fd963a1da5c66309cae0481
Hécate Moonlight <hecate+github@glitchbra.in>
2021-11-29 18:24:01 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wai-middleware-heartbeat
+
+This Wai middleware answers for you on `GET` and `HEAD` requests sent to `/heartbeat`, and will reply with `200 Ok`.
+
+This is a primitive way to do a health check on an API.
---
+++ src/Wai/Middleware/Heartbeat.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Wai.Middleware.Heartbeat (heartbeatMiddleware) where
+
+import Network.HTTP.Types
+import Network.Wai
+
+heartbeatMiddleware :: Middleware
+heartbeatMiddleware app req sendResponse =
+  case rawPathInfo req of
+    "/heartbeat" ->
+      if getVerb
+        then heartbeat
+        else app req sendResponse
+    _ -> app req sendResponse
+  where
+    getVerb = (requestMethod req == methodGet) || (requestMethod req == methodHead)
+    heartbeat = sendResponse $ responseLBS status200 [("Content-Type", "text/plain")] "Ok"
